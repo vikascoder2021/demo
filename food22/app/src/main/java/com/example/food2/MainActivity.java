43f11cb81ccfc0a642a9ccd0fd4f4f54a690f64a
vikascoder2021 <vikascoder2021@gmail.com>
2021-08-28 15:34:02 +0530

    first commit

---
+++ food22/app/src/main/java/com/example/food2/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.food2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
